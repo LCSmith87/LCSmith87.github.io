1a7c0435b6ec4c12fc29462a3f12c2d17adea23a
Levi Smith <LCSmith87@gmail.com>
2016-11-30 15:56:05 -0600

    Portfolio website created
    Portfolio website created

---
+++ js/script.js
@@ -0,0 +1,62 @@
+function toggleMenu () {
+
+	// show/hide the dropdown menu
+	var subMenu = document.getElementById("mobile-nav-items");
+	var isShown = subMenu.style.display;
+
+	subMenu.style.display = isShown == 'none' || isShown == "" ? 'block' : 'none';
+
+}
+
+function scrollToDiv(div, toggle) {
+
+	//Scroll to the appropriate section on the page and hide the menu if open
+	if(toggle) {
+		toggleMenu();
+	}
+	$('html, body').animate({
+        scrollTop: $("#" + div).offset().top
+    }, 1000);
+
+}
+
+function closeOverlay () {
+	// Closes the 'lightbox' and the corresponding modal
+	// Not the most elegant function, but it does the job
+	document.getElementById("dim-overlay").style.display = 'none';
+	document.getElementById("projectCard-tapd").style.display = 'none';
+	document.getElementById("projectCard-solonomostudios").style.display = 'none';
+	document.getElementById("projectCard-frontendpractice").style.display = 'none';
+	document.getElementById("projectCard-adtech").style.display = 'none';
+	document.getElementById("projectCard-filemaker").style.display = 'none';
+	document.getElementById("projectCard-learningpractice").style.display = 'none';
+
+}
+
+function openModal(hasThumbails, div, docEle) {
+	// Get the current scroll position to set the modal to the top
+	var scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+    scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+
+	// Opens the corresponding modal and the 'lightbox'
+	var mod = document.getElementById(div);
+	mod.style.display = 'block';
+	var modWrapper = document.getElementsByClassName("projectCardModalWrapper")[docEle];
+	modWrapper.style.top = scrollTop + "px"
+	document.getElementById("dim-overlay").style.display = 'block';
+	if(hasThumbails) {
+		var divID = div.toString().substring(div.indexOf("-"), div.length);
+		replaceImage("modalMainImg" + divID, document.getElementById("initThumb" + divID));
+	}
+}
+
+function replaceImage(mainImg, div) {
+	var cl = document.getElementsByClassName("thumbIsSelected")[0];
+	
+	if(cl) {
+		cl.removeAttribute("class");
+	}
+	div.setAttribute("class", "thumbIsSelected");
+	document.getElementById(mainImg).setAttribute("src", div.src);
+
+}
